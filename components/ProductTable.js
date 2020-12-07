cc1a5757ef7369f0cf77fbbf2e7ff2403e5cf3cf
ineke.methner <ineke.methner@10316.local>
2020-12-07 10:14:23 +0100

    first commit

---
+++ components/ProductTable.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ProductRow from './ProductRow.js';
+export default class ProductTable extends Component {
+    render() {
+        return (
+                <table>
+          <thead style={{fontWeight: "bold"}}>
+            <tr>
+              <td>Name</td>
+              <td>Price</td>
+              <td>Category</td>
+              <td>in Stock?</td>
+            </tr>
+          </thead>
+<tbody>
+ 
+    <ProductRow products={this.props.products} query={this.props.query} />   
+</tbody>
+
+
+        </table>
+        )
+    }
+}
